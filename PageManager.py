51fcd4295fe85b2f6fa01c28dd1be02d41aa3aed
JaydenStorey <19knowl_L@students.accordsixth.co.uk>
2025-11-27 12:11:12 +0000

    Update PageManager.py

--- PageManager.py
+++ PageManager.py
@@ -3,10 +3,13 @@
 import customtkinter as ctk
 
 class PageManager:
-    def __init__(self, maingui):
+    def __init__(self, rootclass, maingui, rootframe):
         self.currentpage = 0 # placeholder
+        self.lastpage = 0 # placeholder
         self.pages = {}
+        self.rootclass = rootclass
         self.maingui = maingui
+        self.rootframe = rootframe
 
     
     def GetPage(self,pagename):
@@ -18,7 +21,28 @@ class PageManager:
 
     def AddPage(self, pagename, pageclass):
         # defines a page as the page class (each page will be a class)
-        page = pageclass(self.maingui)
+        page = pageclass(self.rootclass, self.maingui, self.rootframe) ## added passing the rootframe
         self.pages[pagename] = page # adds the page in the dictionary under index of the page name
         return page
-    
+
+    def GetLastPage(self):
+        try:
+            return self.pages[self.lastpage]
+        except:
+            print("Last page was not found.")
+            return None
+
+    def ShowPage(self, pagename):
+        page = self.GetPage(pagename)
+
+        # Remove all pages on screen
+        for storedpage in self.pages.values():
+            if hasattr(self, 'startframe'):
+                storedpage.startframe.pack_forget()
+                print("Hid",storedpage)
+
+        if page:
+            page.pack(fill="both", expand=True)
+        else:
+            print("Page was not found")
+            
