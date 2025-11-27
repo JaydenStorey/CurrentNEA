a7e943affe77d8e364c0b6e103b7f459b4f7ce95
JaydenStorey <19knowl_L@students.accordsixth.co.uk>
2025-11-27 12:11:04 +0000

    Update NEA.py

--- NEA.py
+++ NEA.py
@@ -19,21 +19,44 @@ class NEAGui:
         self.frame = ctk.CTkFrame(self.guiroot)
         self.frame.pack(fill="both", expand=True)
 
-        self.PageManager = PageManager(self.guiroot)
+        self.PageManager = PageManager(self, self.guiroot, self.frame)
 
-        self.TestPage = self.PageManager.AddPage("TestPage",TestPage)
-        self.TestPage.pack(fill="both", expand=True)
+        # Add all pages used
+        self.StartPage = self.PageManager.AddPage("StartPage",StartPage)
+        self.LoginPage = self.PageManager.AddPage("LoginPage",LoginPage)
+
+        # Show StartPage
+
+        self.PageManager.ShowPage("StartPage")
 
         self.guiroot.mainloop()
 
-class TestPage(ctk.CTkFrame):
-    def __init__(self, maingui):
+
+class StartPage(ctk.CTkFrame):
+    def __init__(self,rootclass,maingui,rootframe):
         super().__init__(maingui)
 
-        # Put elements of GUI here
-        
-        self.text = ctk.CTkLabel(self, text = "Test text", font = ('Arial', 40))
-        self.text.pack(fill="both", expand=True)
+        # Elements of GUI
+        self.startframe = ctk.CTkFrame(rootframe)
+        self.startframe.pack(fill="both", expand=True)
+
+        self.title = ctk.CTkLabel(self.startframe, text = "Hot Stuff", font = ('Arial', 50), anchor = "center")
+        self.title.pack(anchor = "n")
+
+        self.loginbutton = ctk.CTkButton(self.startframe, text = "Login", command = lambda: rootclass.PageManager.ShowPage("LoginPage"))
+        self.loginbutton.pack(pady = 5)
+
+class LoginPage(ctk.CTkFrame):
+    def __init__(self,rootclass,maingui,rootframe):
+        super().__init__(maingui)
+
+        # Elements of GUI
+        self.startframe = ctk.CTkFrame(rootframe)
+        self.startframe.pack(fill="both", expand=True)
+
+        self.title = ctk.CTkLabel(self.startframe, text = "Login Page", font = ('Arial', 50), anchor = "center")
+        self.title.pack(anchor = "n")
+
 
 
 NEAGui()
